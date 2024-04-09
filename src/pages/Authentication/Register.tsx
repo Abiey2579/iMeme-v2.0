0639fe09b2503f301ec2d07534f69e2a89fd63e9
Abiey2579 <mbyahya2579@gmail.com>
2024-04-09 22:45:18 +0100

    Yahaya - Uninstall Craco

---
+++ src/pages/Authentication/Register.tsx
@@ -0,0 +1,14 @@
+import { IonContent } from "@ionic/react";
+import "./Register.css";
+
+const Register = () => {
+  return (
+    <IonContent>
+      <div className="welcome-background-gradient flex items-end bg-slate-100 h-screen bg-cover bg-center bg-no-repeat">
+        <h1>Register</h1>
+      </div>
+    </IonContent>
+  );
+};
+
+export default Register;
